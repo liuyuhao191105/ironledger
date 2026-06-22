8a05feef4c950c3ca339356a03e20923ef62a278
liuyuhao191105 <liuyuhao191105@gmail.com>
2026-06-22 20:51:20 +0800

    表格标签颜色改回按标签名取色（与弹窗一致）+ 列宽调整

--- js/app.js
+++ js/app.js
@@ -444,7 +444,7 @@ function renderTable(bills) {
     for (var j = 0; j < tags.length; j++) {
       var span = document.createElement('span');
       span.className = 'tag';
-      var color = getTagColorByName(b.category, b.type);
+      var color = getTagColorByName(tags[j], b.type);
       span.style.background = color;
       span.style.color = '#fff';
       span.textContent = tags[j];
